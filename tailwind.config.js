a832297424989dbe5c55accf25696f3fb266de98
abdalrahmanbaker-dev <abdalrahman@gmail.com>
2026-04-01 19:36:04 +0300

    Iitial commit : setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // هذا السطر سيجعل الإضافة تبحث في المجلد الرئيسي مباشرة
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
